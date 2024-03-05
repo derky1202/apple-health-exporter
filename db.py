11eb4f191055e325053f18bcba1638b5c3d740a2
derky1202 <linpengxuan@gmail.com>
2024-03-05 20:09:15 +1100

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
-from sqlalchemy_timescaledb.dialect import TimescaledbDDLCompiler
+from sqlalchemy_timescaledb.dialect import TimescaledbDDLCompiler  # pip install sqlalchemy-timescaledb
 from sqlalchemy import DDL
+import random
+
+
 @staticmethod
 def ddl_hypertable(table_name, hypertable):
-    time_column_name = hypertable['time_column_name']
+    time_column_name = hypertable["time_column_name"]
     partitioning_column = hypertable.get("partitioning_column", None)
     number_partitions = hypertable.get("number_partitions")
-    chunk_time_interval = hypertable.get('chunk_time_interval', '7 days')
+    chunk_time_interval = hypertable.get("chunk_time_interval", "7 days")
 
     if isinstance(chunk_time_interval, str):
         if chunk_time_interval.isdigit():
@@ -14,8 +17,11 @@ def ddl_hypertable(table_name, hypertable):
             chunk_time_interval = f"INTERVAL '{chunk_time_interval}'"
 
     return DDL(
-        f"CREATE UNIQUE INDEX idx_{partitioning_column}_{time_column_name} ON {table_name}({partitioning_column}, {time_column_name});" if partitioning_column else "" +
-        f"""
+        # unique index name 必须唯一，所以加入random打乱，否则报错
+        f"CREATE UNIQUE INDEX idx_{str(random.randint(1,99999))}_{partitioning_column}_{time_column_name} ON {table_name}({partitioning_column}, {time_column_name});"
+        if partitioning_column
+        else ""
+        + f"""
         SELECT create_hypertable(
             '{table_name}',
             '{time_column_name}',
@@ -26,4 +32,6 @@ def ddl_hypertable(table_name, hypertable):
         );
         """
     )
+
+
 TimescaledbDDLCompiler.ddl_hypertable = ddl_hypertable
